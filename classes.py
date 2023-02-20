96ba178ace16cfe5333f6b26c4983bbbec84f368
Rahul-Budhan <Rahulbudhan@gmail.com>
2023-02-20 15:39:52 +0530

    Completed project

--- classes.py
+++ classes.py
@@ -1,8 +1,11 @@
+"""Imports made here"""
 import requests
 from pprint import pprint
 import time
+from datetime import datetime
 import json
 
+"""Get Location names from input file"""
 def get_location():
     handle = open('./docs/locations.txt','r')
     data = handle.read()
@@ -11,19 +14,19 @@ def get_location():
     for line in lines:
         if line[0] == "#" or line[0]== " ":
             lines.remove(line)
-    print(lines)
     return lines
 
-
+"""Connect to server/api and add animation"""
 def connect(url):
-    from_url = url
+    from_url = ''
     page = ''
     while page == '':
         try:
             page = requests.get(from_url)
             break
         except:
-            print("Connecting to server..")
+            """For animation and interaction effect"""
+            print("Connecting to server..\nMight take a few seconds..")
             from_url = url
             a="./   "
             for i in range(30):
@@ -44,13 +47,28 @@ def connect(url):
         print("Connected...")
         time.sleep(1)
         continue
-
     return requests.get(url).json()
 
-def write_details(location,details):
-    handle = open("./docs/details.txt","w")
-    weather_status = details['weather'][0]['main']
-    handle.write(location+":\n")
-    handle.write("Weather: ", weather_status)
-    
+"""Convert Unix time into datetime and store as string"""
+def convert_unix_to_datestr(dt):
+    return datetime.utcfromtimestamp(int(dt)).strftime('%Y-%m-%d %H:%M:%S')
+
+"""Write Output into details.txt"""
+def write_details(details):
+    print("Writing details..")
+    handle = open("./docs/details.txt","a")
+
+    """convert returned unix value to string"""
+    current_time = convert_unix_to_datestr(details['dt'])
+    sunrise_at = convert_unix_to_datestr(details['sys']['sunrise'])
+    time_zone = convert_unix_to_datestr(details['timezone'])
+    sunset_at = convert_unix_to_datestr(details['sys']['sunset'])
+
+    """Format output data"""
+
+    output= ("Location: "+details['name'])+"\n"+("ID :   "+str(details['weather'][0]['id']))+"\n"+("COD :  "+str(details['cod']))+"\n"+("Weather : "+details['weather'][0]['main'])+"\n"+("Wind : "+str(details['wind']['speed']))+"\n"+("Current Time : "+current_time)+"\n"+("Time Zone : "+time_zone)+"\n"+("Humidity: "+str(details['main']['humidity']))+"\n"+("Sunrise At : "+sunrise_at)+"\n"+("Sunset At : "+sunset_at)+"\n"
+
+    """Write output into details.txt"""
+    handle.write(output+"\n")
+
 
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 """Imports made here"""
 from classes import *
 
-
 """Initialization"""
 f = open('./env.json')
 details =json.load(f)
 url = "http://api.openweathermap.org/data/2.5/weather?appid="+details['API_KEY']+"&q="
 
-
 """Locations taken from locations.txt"""
 locations = get_location()
 
+"""Geet details and write details into file for each entry"""
 for location in locations:
     recieved = connect(url+location)
-    write_details(location,recieved)
-
-
+    write_details(recieved)
+print("Execution completed")
